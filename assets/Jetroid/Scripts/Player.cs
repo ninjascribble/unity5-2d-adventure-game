3c98062e6fcb98d716f99fc80ec237454a0ff4cf
Scott Grogan <scott@ninjascript.com>
2017-07-22 14:25:46 -0700

    Add player jump movement

--- assets/Jetroid/Scripts/Player.cs
+++ assets/Jetroid/Scripts/Player.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float speed = 150f;
+	public float jetSpeed = 200f;
 	public Vector2 maxVelocity = new Vector2 (60, 100);
+	public bool standing;
+	public float standingThreshold = 4f;
+	public float airSpeedMultiplier = 0.3f;
 
 	private Rigidbody2D body2d;
 	private SpriteRenderer renderer2d;
@@ -25,27 +29,41 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		var absVelX = Mathf.Abs (body2d.velocity.x);
+		var absVelY = Mathf.Abs (body2d.velocity.y);
 		var forceX = 0f;
 		var forceY = 0f;
 
+		standing = (absVelY <= standingThreshold);
+
 		if (Input.GetKey ("right")) {
 			if (absVelX < maxVelocity.x) {
-				forceX = speed;
+				forceX = standing ? speed : speed * airSpeedMultiplier;
 			}
 			renderer2d.flipX = false;
-			animator.SetInteger ("AnimState", 1);
 		}
 		else if (Input.GetKey ("left")) {
 			if (absVelX < maxVelocity.x) {
-				forceX = -speed;
+				forceX = standing ? -speed : -speed * airSpeedMultiplier;
 			}
 			renderer2d.flipX = true;
-			animator.SetInteger ("AnimState", 1);
 		}
-		else {
-			animator.SetInteger ("AnimState", 0);
+
+		if (Input.GetKey ("up")) {
+			if (absVelY < maxVelocity.y) {
+				forceY = jetSpeed;
+			}
 		}
 
 		body2d.AddForce (new Vector2 (forceX, forceY));
+
+		if (!standing && Input.GetKey ("up")) {
+			animator.SetInteger ("AnimState", 2); // jet
+		}
+		else if (absVelX > standingThreshold) {
+			animator.SetInteger ("AnimState", 1); // walk
+		}
+		else {
+			animator.SetInteger ("AnimState", 0); // idle
+		}
 	}
 }
