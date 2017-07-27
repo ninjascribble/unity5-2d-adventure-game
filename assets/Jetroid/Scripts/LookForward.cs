e6a461a3e25d8dcde7d793cf23e17329793e79da
Scott Grogan <scott@ninjascript.com>
2017-07-26 21:23:03 -0700

    Alien avoids gaps

--- assets/Jetroid/Scripts/LookForward.cs
+++ assets/Jetroid/Scripts/LookForward.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LookForward : MonoBehaviour {
 
 	public Transform sightStart, sightEnd;
+	public string layer = "Solid";
+	public bool needsCollision = true;
 
 	private bool collision = false;
 
@@ -15,11 +17,11 @@ public class LookForward : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		collision = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("Solid"));
+		collision = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer (layer));
 
 		Debug.DrawLine (sightStart.position, sightEnd.position, Color.green);
 
-		if (collision == true) {
+		if (collision == needsCollision) {
 			transform.localScale = new Vector3 (transform.localScale.x * -1, 1, 1);
 		}
 	}
